6e8a861c635df44a3b172dd7e12f21a605a755a5
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-01 13:31:38 +0330

    feat(Application): Create OperationResult

---
+++ Application/Common/OperationResult1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.Common
+{
+    public class OperationResult
+    {
+        public bool Success { get;  set; }
+        public bool Result { get; set; }
+        public string? Message { get;  set; }
+        public Exception? Exception { get;  set; }
+
+        public static OperationResult SuccessedResult(bool success= true,
+            string message = "")
+        {
+            return new OperationResult
+            {
+                Success = success,
+                Message = message
+                ,Result=true
+            };
+
+        }
+        public static OperationResult Failed(
+            string message = "", Exception? ex=null)
+        {
+            return new OperationResult
+            {
+                Success = false,
+                Message = message,
+                Exception=ex,
+                Result=false
+            };
+        }
+        public static OperationResult FailedResult(
+           string message = "")
+        {
+            return new OperationResult
+            {
+                Success = false,
+                Message = message,
+                Result=true
+            };
+        }
+
+    }
+}
---
+++ Application/Common/OperationResult2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SabzMarket.Application.Common
+{
+    public class OperationResult<t>: OperationResult
+    {
+            public t Data { get; set; }
+            public static OperationResult<t> SuccessedResult(
+                 t data, string message = "")
+            {
+                return new OperationResult<t>
+                {
+                    Success = true,
+                    Message = message,
+                    Data = data
+                    ,Result=true
+                };
+            }
+        public static OperationResult<t> Failed(
+           string message = "", Exception? ex = null)
+        {
+            return new OperationResult<t>
+            {
+                Success = false,
+                Message = message,
+                Exception = ex,
+                Result = false
+            };
+        }
+        public static OperationResult<t> FailedResult(
+                string message = "")
+            {
+                return new OperationResult<t>
+                {
+                    Success = false,
+                    Message = message,
+                   Result=true
+                };
+            }
+    }
+}
