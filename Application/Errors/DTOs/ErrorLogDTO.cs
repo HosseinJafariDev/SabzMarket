7726008fde8169600821a7975cdf3aadf576ef33
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-01 13:32:07 +0330

    feat(Application): Create ErrorDTO

---
+++ Application/Errors/DTOs/ErrorLogDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SabzMarket.Application.Errors
+{
+    public class ErrorLogDTO
+    {
+        public int Id { get; set; }
+        public string? Message { get; set; }
+        public string? StackTrace { get; set; }
+        public string? Source { get; set; }
+        public string? Layer { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string? Curl { get; set; }
+        public string? Route { get; set; }
+    }
+}
