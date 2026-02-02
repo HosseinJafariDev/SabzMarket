482252034c98eb4e0a37ba05e36f46c19428bfbb
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-02 14:18:41 +0330

    feat(Application):Create SignUpProfile

---
+++ Application/UseCases/Auth/Mappers/SignUpProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using SabzMarket.Application.UseCases.Auth.UseCases.SignUp;
+using SabzMarket.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.Auth.Mappers
+{
+    public class SignUpProfile:Profile
+    {
+        public SignUpProfile()
+        {
+            CreateMap<SignUpInputDTO,User>();
+        }
+    }
+}
