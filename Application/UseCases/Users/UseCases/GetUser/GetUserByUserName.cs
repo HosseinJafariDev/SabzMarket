e560f60a44959754ede549db3b161df8b5053836
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-02 23:57:03 +0330

    refactor(Application):add GetUserByUserName classes

---
+++ Application/UseCases/Users/UseCases/GetUser/GetUserByUserName.cs
@@ -0,0 +1,49 @@
+﻿using Application.Interfaces.Services;
+using AutoMapper;
+using SabzMarket.Application.Common;
+using SabzMarket.Application.Interfaces.Repository;
+using SabzMarket.Application.UseCases.Users.UseCases.CheckUsernameAvailability;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.Users.UseCases.GetUser
+{
+    public class GetUserByUserName : IGetUserByUserName
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IErrorService _errorService;
+        private readonly IMapper _mapper;
+        public GetUserByUserName(IUserRepository userRepository, IErrorService errorService, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _errorService = errorService;
+            _mapper = mapper;
+        }
+
+        public async Task<OperationResult<GetUserByUserNameOutputDTO>> ExecuteAsync(string username)
+        {
+            if (string.IsNullOrWhiteSpace(username))
+                return OperationResult<GetUserByUserNameOutputDTO>.FailedResult(Messages.UserNameMinLength);
+
+            try
+            {
+                var result = await _userRepository.SelectByUserNameAsync(username);
+                if (result == null)
+                {
+                    return OperationResult<GetUserByUserNameOutputDTO>.FailedResult(Messages.UserNotFound);
+                }
+                var userDTO = _mapper.Map<GetUserByUserNameOutputDTO>(result);
+                return OperationResult<GetUserByUserNameOutputDTO>.SuccessedResult(userDTO);
+            }
+            catch (Exception ex)
+            {
+                var errorResult = await _errorService.LogErrorAsync(ex, GetType().Name);
+                return OperationResult<GetUserByUserNameOutputDTO>.Failed(errorResult.Message!.ErrorMessage());
+            }
+
+        }
+    }
+}
--- Application/UserService.cs
+++ Application/UserService.cs
@@ -27,80 +27,80 @@ namespace SabzMarket.Application
             _errorService = errorService;
         }
 
-        public async Task<OperationResult<UserViewModel>> GetUserAsync(string username)
-        {
-            var result=await _userRepository.SelectAsync(username);
-            if(!result.Success)
-            {
-                var error = result.Exception!.ExceptionToErrorDTO(result.Message!);
-                var result1 = await _errorService.LogErrorAsync(error);
-                return OperationResult<UserViewModel>.Failed(result1.Message!.ErrorMessage());
-            }
-            var data = result.Data.ToUserViewModel();
-            return OperationResult<UserViewModel>.SuccessedResult(data);
-        }
+        //public async Task<OperationResult<UserViewModel>> GetUserAsync(string username)
+        //{
+        //    var result=await _userRepository.SelectAsync(username);
+        //    if(!result.Success)
+        //    {
+        //        var error = result.Exception!.ExceptionToErrorDTO(result.Message!);
+        //        var result1 = await _errorService.LogErrorAsync(error);
+        //        return OperationResult<UserViewModel>.Failed(result1.Message!.ErrorMessage());
+        //    }
+        //    var data = result.Data.ToUserViewModel();
+        //    return OperationResult<UserViewModel>.SuccessedResult(data);
+        //}
 
-        public async Task<OperationResult> IsUsernameAvailableAsync(string username)
-        {
-            var result = await _userRepository.CheckUserAsync(username);
-            if (!result.Success)
-            {
-                if(!result.Result)
-                {
-                    var error = result.Exception!.ExceptionToErrorDTO(result.Message!);
-                    var result1 = await _errorService.LogErrorAsync(error);
-                    return OperationResult.Failed(result1.Message!.ErrorMessage());
-                }
-                return OperationResult.FailedResult();
-            }
-            return OperationResult.SuccessedResult(true, Messages.existingUser);
+        //public async Task<OperationResult> IsUsernameAvailableAsync(string username)
+        //{
+        //    var result = await _userRepository.CheckUserAsync(username);
+        //    if (!result.Success)
+        //    {
+        //        if(!result.Result)
+        //        {
+        //            var error = result.Exception!.ExceptionToErrorDTO(result.Message!);
+        //            var result1 = await _errorService.LogErrorAsync(error);
+        //            return OperationResult.Failed(result1.Message!.ErrorMessage());
+        //        }
+        //        return OperationResult.FailedResult();
+        //    }
+        //    return OperationResult.SuccessedResult(true, Messages.existingUser);
 
             
-        }
+        //}
 
-        public async Task<OperationResult> LoginAsync(UserViewModel userViewModel)
-        {
-            var result=await _userRepository.ChangePasswordAsync(userViewModel.ToUserDTO());
-            if (!result.Success)
-            {
-                if(!result.Result)
-                {
-                    var error = result.Exception!.ExceptionToErrorDTO(result.Message!);
-                    var result1=await _errorService.LogErrorAsync(error);
-                    return OperationResult.Failed(result1.Message!.ErrorMessage());
-                }
-                return OperationResult.FailedResult(Messages.userNotLogin);
-            }
-            return OperationResult.SuccessedResult();
+        //public async Task<OperationResult> LoginAsync(UserViewModel userViewModel)
+        //{
+        //    var result=await _userRepository.ChangePasswordAsync(userViewModel.ToUserDTO());
+        //    if (!result.Success)
+        //    {
+        //        if(!result.Result)
+        //        {
+        //            var error = result.Exception!.ExceptionToErrorDTO(result.Message!);
+        //            var result1=await _errorService.LogErrorAsync(error);
+        //            return OperationResult.Failed(result1.Message!.ErrorMessage());
+        //        }
+        //        return OperationResult.FailedResult(Messages.userNotLogin);
+        //    }
+        //    return OperationResult.SuccessedResult();
             
-        }
+        //}
 
-        public async Task<OperationResult> SignUpAsync(UserViewModel userViewModel)
-        {
-            if(!userViewModel.IsValid)
-            {
-                return OperationResult.FailedResult(userViewModel.ErrorMessage);
-            }
-            var result = await _userRepository.CheckUserAsync(userViewModel.UserName!);
-            if (!result.Success)
-            {
-                if(!result.Result)
-                {
-                    var error = result.Exception!.ExceptionToErrorDTO(result.Message!);
-                    var result1 = await _errorService.LogErrorAsync(error);
-                    return OperationResult.Failed(result1.Message!.ErrorMessage());
-                }
-                var user = userViewModel.ToUserDTO();
-                var result2 = await _userRepository.InsertAsync(user);
-                if(!result2.Success)
-                {
-                    var error = result2.Exception!.ExceptionToErrorDTO(result2.Message!);
-                    var result1 = await _errorService.LogErrorAsync(error);
-                    return OperationResult.Failed(result1.Message!.ErrorMessage());
-                }
-                return OperationResult.SuccessedResult(true, Messages.successSignUp1);
-            }
-            return OperationResult.FailedResult(Messages.existingUser);
-        }
+        //public async Task<OperationResult> SignUpAsync(UserViewModel userViewModel)
+        //{
+        //    if(!userViewModel.IsValid)
+        //    {
+        //        return OperationResult.FailedResult(userViewModel.ErrorMessage);
+        //    }
+        //    var result = await _userRepository.CheckUserAsync(userViewModel.UserName!);
+        //    if (!result.Success)
+        //    {
+        //        if(!result.Result)
+        //        {
+        //            var error = result.Exception!.ExceptionToErrorDTO(result.Message!);
+        //            var result1 = await _errorService.LogErrorAsync(error);
+        //            return OperationResult.Failed(result1.Message!.ErrorMessage());
+        //        }
+        //        var user = userViewModel.ToUserDTO();
+        //        var result2 = await _userRepository.InsertAsync(user);
+        //        if(!result2.Success)
+        //        {
+        //            var error = result2.Exception!.ExceptionToErrorDTO(result2.Message!);
+        //            var result1 = await _errorService.LogErrorAsync(error);
+        //            return OperationResult.Failed(result1.Message!.ErrorMessage());
+        //        }
+        //        return OperationResult.SuccessedResult(true, Messages.successSignUp1);
+        //    }
+        //    return OperationResult.FailedResult(Messages.existingUser);
+        //}
     }
 }
