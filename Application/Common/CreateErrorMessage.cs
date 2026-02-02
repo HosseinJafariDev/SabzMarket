e3a152ba9755d1ecbb0e569f549e869a7c00dce4
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-02 14:15:45 +0330

    feat: add  class CreateErrorMessage

---
+++ Application/Common/CreateErrorMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.Common
+{
+    public static class CreateErrorMessage
+    {
+        public static string ErrorMessage(this string message)
+        {
+            return string.Format($"{Messages.Error}{Environment.NewLine}{Messages.CodeError}{message}");
+
+        }
+    }
+}
