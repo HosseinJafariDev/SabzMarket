cf24dce6dcb84cfc16eea753bd24e76cb9808c9f
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-02 13:43:31 +0330

    feat(Domain):Add VerifyPassword Method to User Entity

--- SabzMarket.Domain/Entities/User.cs
+++ SabzMarket.Domain/Entities/User.cs
@@ -16,5 +16,9 @@ namespace SabzMarket.Domain.Entities
         public string? Email { get; set; }
         public string? UserName { get; set; }
         public string? Password { get; set; }
+        public bool VerifyPassword(string password)
+        {
+            return Password == password;
+        }
     }
 }
