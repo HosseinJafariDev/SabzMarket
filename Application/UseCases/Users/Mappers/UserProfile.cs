014555892a3baafb50ea026499343631f38bff61
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-02 23:51:14 +0330

    refactor(Application):add UserProfile

---
+++ Application/UseCases/Users/Mappers/UserProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using SabzMarket.Application.UseCases.Users.DTOs;
+using SabzMarket.Application.UseCases.Users.UseCases.GetUser;
+using SabzMarket.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.Users.Mappers
+{
+    public class UserProfile:Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User,GetUserByUserNameOutputDTO>();
+            CreateMap<GetUserByUserNameOutputDTO, User>();
+        }
+    }
+}
