b57e51f47316950d2433551cb9ef6ea25c230df3
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-10 23:40:48 +0330

    feat(Application):add ICreateProductUseCase,CreateProductValidator,CreateProductUseCase,CreateProductInputDTO

---
+++ Application/UseCases/Products/CreateProduct/CreateProductUseCase.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using FluentValidation;
+using SabzMarket.Application.Common;
+using SabzMarket.Application.Interfaces.Repositories.Services;
+using SabzMarket.Application.Interfaces.Repository;
+using SabzMarket.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.Products.CreateProduct
+{
+    public class CreateProductUseCase : ICreateProductUseCase
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IFileStorageService _fileStorageService;
+        private readonly IErrorRepository _errorRepository;
+        private readonly IMapper _mapper;
+        private readonly IValidator<CreateProductInputDTO> _validator;
+
+        public CreateProductUseCase(
+            IProductRepository productRepository,
+            IFileStorageService fileStorageService,
+            IErrorRepository errorRepository,
+            IMapper mapper,
+            IValidator<CreateProductInputDTO> validator)
+        {
+            _errorRepository = errorRepository;
+            _productRepository = productRepository;
+            _fileStorageService = fileStorageService;
+            _mapper = mapper;
+            _validator = validator;
+        }
+        public async Task<OperationResult> ExecuteAsync(CreateProductInputDTO createProductInputDTO)
+        {
+            try
+            {
+                var validationResult = _validator.Validate(createProductInputDTO);
+                if (validationResult.IsValid)
+                {
+                    return OperationResult.FailedResult(validationResult.Errors.First().ErrorMessage);
+                }
+
+                var imageUrl = await _fileStorageService.SaveAsync(createProductInputDTO.ImageProduct!);
+                createProductInputDTO.ImageProduct = imageUrl;
+
+                var product = _mapper.Map<Product>(createProductInputDTO);
+                await _productRepository.InsertAsync(product);
+
+                return OperationResult.SuccessedResult(true, Messages.CreateProductSuccessful);
+            }
+            catch (Exception ex)
+            {
+                var errorResult = await _errorRepository.LogErrorAsync(ex, GetType().Name);
+                return OperationResult.Failed(errorResult.ErrorMessage());
+            }
+
+
+        }
+    }
+}
---
+++ Application/UseCases/Products/CreateProduct/ICreateProductUseCase.cs
@@ -0,0 +1,14 @@
+﻿using SabzMarket.Application.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.Products.CreateProduct
+{
+    public interface ICreateProductUseCase
+    {
+        Task<OperationResult> ExecuteAsync(CreateProductInputDTO createProductInputDTO);
+    }
+}
