a34fcc336ddf2851c75d6a4358f29afe69f2c9b1
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-10 23:19:32 +0330

    rename productname to name

--- SabzMarket.Domain/Entities/Product.cs
+++ SabzMarket.Domain/Entities/Product.cs
@@ -8,12 +8,12 @@ using System.Threading.Tasks;
 
 namespace SabzMarket.Domain.Entities
 {
-    public class    Product
+    public class Product
     {
         public long Id { get; set; }
         public long SellerId { get; set; }
         public long CategorieId { get; set; }
-        public string? ProductName { get; set; }
+        public string? Name { get; set; }
         public string? Description { get; set; }
         public int Price { get; set; }
         public int Number { get; set; }
