f8a285da9902451cccd73b13cdfe3bd2b797229f
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-10 23:40:20 +0330

    feat(Application):add IDeleteProductUseCase,DeleteProductUseCase

---
+++ Application/UseCases/Products/DeleteProduct/DeleteProductUseCase.cs
@@ -0,0 +1,43 @@
+﻿using SabzMarket.Application.Common;
+using SabzMarket.Application.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.Products.DeleteProduct
+{
+    public class DeleteProductUseCase : IDeleteProductUseCase
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IOrderDetailRepository _orderDetailRepository;
+        private readonly IErrorRepository _errorRepository;
+        public DeleteProductUseCase(IProductRepository productRepository, IOrderDetailRepository orderDetailRepository, IErrorRepository errorRepository)
+        {
+            _orderDetailRepository = orderDetailRepository;
+            _productRepository = productRepository;
+            _errorRepository = errorRepository;
+        }
+        public async Task<OperationResult> ExecuteAsync(long id)
+        {
+            try
+            {
+                var hasPendingOrders = await _orderDetailRepository.HasPendingOrdersForProductAsync(id);
+                if (hasPendingOrders)
+                {
+                    return OperationResult.FailedResult(Messages.ProductIsOnOrder);
+                }
+
+                await _productRepository.DeleteAsync(id);
+                return OperationResult.SuccessedResult(true, Messages.ProductDelete);
+            }
+            catch (Exception ex)
+            {
+                var errorResult = await _errorRepository.LogErrorAsync(ex, GetType().Name);
+                return OperationResult.Failed(errorResult.ErrorMessage());
+            }
+        }
+    }
+}
---
+++ Application/UseCases/Products/DeleteProduct/IDeleteProductUseCase.cs
@@ -0,0 +1,14 @@
+﻿using SabzMarket.Application.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.Products.DeleteProduct
+{
+    public interface IDeleteProductUseCase
+    {
+        Task<OperationResult> ExecuteAsync(long id);
+    }
+}
