0989d01d98dad289ac8bde6567e0cb7a8e203758
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-25 22:23:08 +0330

    Add GetFarmerByUsernameOutputDTO , GetFarmerByUsernameUseCase , IFarmerQueryService and IGetFarmerByUsernameUseCase

---
+++ Application/UseCases/Farmers/GetFarmer/GetFarmerByUsernameUseCase.cs
@@ -0,0 +1,34 @@
+﻿using SabzMarket.Application.Common;
+using SabzMarket.Application.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.Farmers.GetFarmer
+{
+    public class GetFarmerByUsernameUseCase : IGetFarmerByUsernameUseCase
+    {
+        private readonly IFarmerQueryService _farmerQueryService;
+        private readonly IErrorRepository _errorRepository;
+        public GetFarmerByUsernameUseCase(IFarmerQueryService farmerQueryService, IErrorRepository errorRepository)
+        {
+            _errorRepository = errorRepository;
+            _farmerQueryService = farmerQueryService;
+        }
+        public async Task<OperationResult<GetFarmerByUsernameOutputDTO>> ExecuteAsync(string username)
+        {
+            try
+            {
+                var farmer = await _farmerQueryService.SelectByUsernameAsync(username);
+                return OperationResult<GetFarmerByUsernameOutputDTO>.SuccessedResult(farmer);
+            }
+            catch (Exception ex)
+            {
+                var errorResult = await _errorRepository.LogErrorAsync(ex.ExceptionToErrorDTO(GetType().Name));
+                return OperationResult<GetFarmerByUsernameOutputDTO>.Failed(errorResult.ErrorMessage());
+            }
+        }
+    }
+}
