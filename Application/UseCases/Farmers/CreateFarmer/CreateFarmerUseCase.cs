d27d0b51a22402822cbd09353df84c54eddaad84
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-25 22:23:50 +0330

    Add CreateFarmerInputDTO , CreateFarmerUseCase , CreateFarmerValidator and ICreateFarmerUseCase

---
+++ Application/UseCases/Farmers/CreateFarmer/CreateFarmerUseCase.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using FluentValidation;
+using SabzMarket.Application.Common;
+using SabzMarket.Application.Interfaces.Repositories.Services;
+using SabzMarket.Application.Interfaces.Repository;
+using SabzMarket.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.Farmers.CreateFarmer
+{
+    public class CreateFarmerUseCase : ICreateFarmerUseCase
+    {
+        private readonly IErrorRepository _errorRepository;
+        private readonly IFarmerRepository _farmerRepository;
+        private readonly IValidator<CreateFarmerInputDTO> _validator;
+        private readonly IFileStorageService _fileStorageService;
+        private readonly IMapper _mapper;
+        public CreateFarmerUseCase(
+            IErrorRepository errorRepository,
+            IFarmerRepository farmerRepository,
+            IValidator<CreateFarmerInputDTO> validator,
+            IFileStorageService fileStorageService,
+            IMapper mapper)
+        {
+            _errorRepository = errorRepository;
+            _farmerRepository = farmerRepository;
+            _validator = validator;
+            _fileStorageService = fileStorageService;
+            _mapper = mapper;
+        }
+        public async Task<OperationResult> ExecuteAsync(string username, CreateFarmerInputDTO createFarmerInputDTO)
+        {
+            try
+            {
+                var farmerValidation = _validator.Validate(createFarmerInputDTO);
+                if (!farmerValidation.IsValid)
+                {
+                    return OperationResult.FailedResult(farmerValidation.Errors.First().ErrorMessage);
+                }
+
+                var imageURL = await _fileStorageService.SaveAsync(createFarmerInputDTO.ProfileImage!);
+                createFarmerInputDTO.ProfileImage = imageURL;
+
+                var farmer = _mapper.Map<Farmer>(createFarmerInputDTO);
+                await _farmerRepository.InsertAsync(username, farmer);
+                return OperationResult.SuccessedResult(true, Messages.SignUpSuccessful);
+            }
+            catch (Exception ex)
+            {
+                var errorResult = await _errorRepository.LogErrorAsync(ex.ExceptionToErrorDTO(GetType().Name));
+                return OperationResult.Failed(errorResult.ErrorMessage());
+            }
+        }
+    }
+}
---
+++ Application/UseCases/Farmers/CreateFarmer/CreateFarmerValidator.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+using SabzMarket.Application.Common;
+using SabzMarket.Application.Common.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.Farmers.CreateFarmer
+{
+    public class CreateFarmerValidator : AbstractValidator<CreateFarmerInputDTO>
+    {
+        public CreateFarmerValidator()
+        {
+
+            RuleFor(x => x.Address)
+                .NotEmpty().WithMessage(Messages.AddressRequired)
+                .MaximumLength(500).WithMessage(Messages.AddressMaxLength);
+
+            RuleFor(x => x.DataBuilt)
+                .Length(10).WithMessage(Messages.EnterDataBuilt);
+
+            RuleFor(x => x.LandArea)
+                .GreaterThan(0).WithMessage(Messages.EnterLandArea);
+
+            RuleFor(x => x.NationalCode)
+                .NotEmpty().WithMessage(Messages.NationalCodeRequired)
+                .Must(NationalCodeValidator.IsValid!).WithMessage(Messages.NotValidNationalCode);
+
+            RuleFor(x => x.CodParvaneBHB)
+                .Length(14).WithMessage(Messages.EnterCodParvaneBHB);
+
+            RuleFor(x => x.ProfileImage)
+                .NotEmpty().WithMessage(Messages.ProfileImageRequired);
+
+            RuleFor(x => x.CodePosti)
+                .Length(10).WithMessage(Messages.EnterCodePosti);
+        }
+    }
+}
