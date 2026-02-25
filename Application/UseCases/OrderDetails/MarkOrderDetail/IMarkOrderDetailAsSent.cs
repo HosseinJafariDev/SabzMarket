859f46072203a934f34a5418d9df470acd3ddbff
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-25 22:16:03 +0330

    Add IMarkOrderDetailAsSent MarkOrderDetailAsSent

---
+++ Application/UseCases/OrderDetails/MarkOrderDetail/IMarkOrderDetailAsSent.cs
@@ -0,0 +1,14 @@
+﻿using SabzMarket.Application.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.OrderDetails.MarkOrderDetail
+{
+    public interface IMarkOrderDetailAsSent
+    {
+        Task<OperationResult> ExecuteAsync(long orderDetaileId);
+    }
+}
---
+++ Application/UseCases/OrderDetails/MarkOrderDetail/MarkOrderDetailAsSent.cs
@@ -0,0 +1,34 @@
+﻿using SabzMarket.Application.Common;
+using SabzMarket.Application.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.OrderDetails.MarkOrderDetail
+{
+    public class MarkOrderDetailAsSent : IMarkOrderDetailAsSent
+    {
+        private readonly IErrorRepository _errorRepository;
+        private readonly IOrderDetailRepository _orderDetailRepository;
+        public MarkOrderDetailAsSent(IErrorRepository errorRepository, IOrderDetailRepository orderDetailRepository)
+        {
+            _errorRepository = errorRepository;
+            _orderDetailRepository = orderDetailRepository;
+        }
+        public async Task<OperationResult> ExecuteAsync(long orderDetaileId)
+        {
+            try
+            {
+                await _orderDetailRepository.SetOrderDetailStatusToSentAsync(orderDetaileId);
+                return OperationResult.SuccessedResult(true, Messages.OrderSent);
+            }
+            catch (Exception ex)
+            {
+                var errorReslt = await _errorRepository.LogErrorAsync(ex.ExceptionToErrorDTO(GetType().Name));
+                return OperationResult.Failed(errorReslt.ErrorMessage());
+            }
+        }
+    }
+}
