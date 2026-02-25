24524ab9fbe6085d714437142b1cd0085cd133d9
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-25 22:34:26 +0330

    Add CategorieProfile

---
+++ Application/UseCases/Categories/Mappers/CategorieProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using SabzMarket.Application.UseCases.Categories.GetCategory;
+using SabzMarket.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.Categories.Mappers
+{
+    public class CategorieProfile : Profile
+    {
+        public CategorieProfile()
+        {
+            CreateMap<Categorie, GetAllCategoriesOutputDTO>();
+        }
+    }
+}
