26550729591e32917a8337579f3aa482ecfcf462
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-25 22:12:40 +0330

    Add GetOrdersForSellerOutputDTO

---
+++ Application/UseCases/Orders/GetOrders/GetOrdersForSellerOutputDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.Orders.GetOrders
+{
+    public class GetOrdersForSellerOutputDTO
+    {
+        public long OrderId { get; set; }
+        public long OrderDetailId { get; set; }
+        public long ProductId { get; set; }
+        public string? ImageProduct { get; set; }
+        public string? ProductName { get; set; }
+        public string? Status { get; set; }
+        public int Number { get; set; }
+        public long FarmerId { get; set; }
+        public string? Address { get; set; }
+        public string? FarmerProfileImage { get; set; }
+        public string? Phone { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? CodePosti { get; set; }
+    }
+}
