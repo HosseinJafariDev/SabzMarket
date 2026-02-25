4e4f99a2dde5a08bfbbe4b1a1b102e4f7dbee66d
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-25 22:17:29 +0330

    Add     public class GetAllFeaturedSellerOutputDTO

---
+++ Application/UseCases/FeaturedSellers/GetFeaturedSeller/GetAllSellerOutputDTO.cs
@@ -0,0 +1,18 @@
+﻿using SabzMarket.Application.UseCases.Auth.Login;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.FeaturedSellers.GetFeaturedSeller
+{
+    public class GetAllFeaturedSellerOutputDTO
+    {
+        public long SellerId { get; set; }
+        public long UserId { get; set; }
+        public string? ProfileImage { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
