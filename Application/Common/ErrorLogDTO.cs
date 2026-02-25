253b3ce6b8562ae50ae57e5824bde425bf846e37
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-25 21:01:22 +0330

    Add ErrorLogDTO and ErrorMapper

---
+++ Application/Common/ErrorLogDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.Common
+{
+    public class ErrorLogDTO
+    {
+        public string? Message { get; set; }
+        public string? StackTrace { get; set; }
+        public string? Source { get; set; }
+        public string? Layer { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string? Curl { get; set; }
+        public string? Route { get; set; }
+    }
+}
---
+++ Application/Common/ErrorMapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.Common
+{
+    public static class ErrorMapper
+    {
+        public static ErrorLogDTO ExceptionToErrorDTO(this Exception ex, String layer)
+        {
+            return new ErrorLogDTO
+            {
+                Layer = layer,
+                Message = ex.InnerException?.Message ?? ex.Message,
+                Source = ex.Source,
+                StackTrace = ex.StackTrace,
+            };
+        }
+    }
+}
