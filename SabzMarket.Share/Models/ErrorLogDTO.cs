f268d6d6f03d85b5dbe37ae94ee6fcbb1206cbbf
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-25 19:55:57 +0330

    refactor Remove Id property

--- SabzMarket.Share/Models/ErrorLogDTO.cs
+++ SabzMarket.Share/Models/ErrorLogDTO.cs
@@ -6,7 +6,6 @@ namespace SabzMarket.Share.Models
 {
     public class ErrorLogDTO
     {
-        public int Id { get; set; }
         public string? Message { get; set; }
         public string? StackTrace { get; set; }
         public string? Source { get; set; }
