a5f27f906086731ad3faefdc6fdf545b30fd3c9e
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-25 22:13:19 +0330

    Add IOrderQueryService

---
+++ Application/UseCases/Orders/GetOrders/IOrderQueryService.cs
@@ -0,0 +1,15 @@
+﻿using SabzMarket.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.Orders.GetOrders
+{
+    public interface IOrderQueryService
+    {
+        Task<List<GetOrdersForSellerOutputDTO>> SelectPendingOrdersForSellerAsync(long sellerId, string search);
+        Task<List<GetOrdersForSellerOutputDTO>> SelectNonPendingOrdersForSellerAsync(long sellerId, string search);
+    }
+}
