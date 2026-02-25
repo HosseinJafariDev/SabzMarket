a86ba158fc8e33fd1ba661b63e4dc798f98badaf
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-25 22:13:33 +0330

    Add OrderProfile

---
+++ Application/UseCases/Orders/Mappers/OrderProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using SabzMarket.Application.UseCases.CartItems.GetCartItem;
+using SabzMarket.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.Orders.Mappers
+{
+    public class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<GetCartItemByFarmerIdOutputDTO, Order>()
+                .ForMember(dest => dest.OrderDate, opt => opt.MapFrom(x => x.AddedDate));
+
+            CreateMap<GetCartItemByFarmerIdOutputDTO, OrderDetail>()
+                .ForMember(dest => dest.OrderId, opt => opt.MapFrom(x => x.Id))
+                .ForMember(dest => dest.Price, opt => opt.MapFrom(x => x.ProductPrice))
+                .ForMember(dest => dest.Number, opt => opt.MapFrom(x => x.ProducNumber));
+        }
+    }
+}
