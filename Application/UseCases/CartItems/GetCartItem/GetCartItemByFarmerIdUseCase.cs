4cf6d2d6a7d5845505aa7eb41a69bf3b0d22b31f
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-25 22:32:31 +0330

    Add GetCartItemByFarmerIdOutputDTO , GetCartItemByFarmerIdUseCase , ICartItemQueryService and IGetCartItemByFarmerIdUseCase

---
+++ Application/UseCases/CartItems/GetCartItem/GetCartItemByFarmerIdUseCase.cs
@@ -0,0 +1,46 @@
+﻿using Application.Interfaces.Repositories;
+using SabzMarket.Application.Common;
+using SabzMarket.Application.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.CartItems.GetCartItem
+{
+    public class GetCartItemByFarmerIdUseCase : IGetCartItemByFarmerIdUseCase
+    {
+        private readonly IErrorRepository _errorRepository;
+        private readonly ICartItemQueryService _cartItemQueryService;
+        private readonly ICartItemRepository _cartItemRepository;
+        public GetCartItemByFarmerIdUseCase(IErrorRepository errorRepository,
+            ICartItemQueryService cartItemQueryService,
+            ICartItemRepository cartItemRepository)
+        {
+            _errorRepository = errorRepository;
+            _cartItemQueryService = cartItemQueryService;
+            _cartItemRepository = cartItemRepository;
+        }
+        public async Task<OperationResult<List<GetCartItemByFarmerIdOutputDTO>>> Execute(long farmerId)
+        {
+            try
+            {
+                var carts = await _cartItemQueryService.SelectByFarmerIdAsync(farmerId);
+                var data = carts.Where(x => x.Quantity > x.ProducNumber).ToList();
+                foreach (var item in data)
+                {
+                    await _cartItemRepository.DeleteAsync(item.Id);
+                }
+                carts.RemoveAll(x => x.Quantity > x.ProducNumber);
+
+                return OperationResult<List<GetCartItemByFarmerIdOutputDTO>>.SuccessedResult(carts);
+            }
+            catch (Exception ex)
+            {
+                var errorResult = await _errorRepository.LogErrorAsync(ex.ExceptionToErrorDTO(GetType().Name));
+                return OperationResult<List<GetCartItemByFarmerIdOutputDTO>>.Failed(errorResult.ErrorMessage());
+            }
+        }
+    }
+}
---
+++ Application/UseCases/CartItems/GetCartItem/ICartItemQueryService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.CartItems.GetCartItem
+{
+    public interface ICartItemQueryService
+    {
+        public Task<List<GetCartItemByFarmerIdOutputDTO>> SelectByFarmerIdAsync(long farmerId);
+    }
+}
