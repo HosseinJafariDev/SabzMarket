476e345521da1641020c603dd550e056830a7c2b
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-25 20:21:10 +0330

    refactor: move exception mapping to application layer

--- SabzMarket.Infrastructure/Persistence/Repository/ErrorRepository.cs
+++ SabzMarket.Infrastructure/Persistence/Repository/ErrorRepository.cs
@@ -1,44 +1,35 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using SabzMarket.Application.Errors;
+using SabzMarket.Application.Common;
 using SabzMarket.Application.Interfaces.Repository;
 using SabzMarket.Application.Interfaces.Services;
-using SabzMarket.Domain.Entities;
 using SabzMarket.Infrastructure.Entities;
-using SabzMarket.Infrastructure.Mappers;
-using SabzMarket.Infrastructure.Persistence;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SabzMarket.Infrastructure.Persistence.Repository
 {
-    public class ErrorRepository:IErrorRepository
+    public class ErrorRepository : IErrorRepository
     {
         private readonly IDbContextFactory<SabzMarketDbContext> _contextFactory;
         private readonly IFileLogService _fileLogService;
 
         public ErrorRepository
             (IDbContextFactory<SabzMarketDbContext> contextFactory,
-            IFileLogService fileLogService) 
+            IFileLogService fileLogService)
         {
             _contextFactory = contextFactory;
             _fileLogService = fileLogService;
         }
 
-        public async Task<string> LogErrorAsync(Exception ex, String layer)
+        public async Task<string> LogErrorAsync(ErrorLogDTO errorLogDTO)
         {
-            var error = ex.ExceptionToErrorDTO(layer);
             var errorLog = new ErrorTable
             {
-                CreatedAt = error.CreatedAt,
-                Message = error.Message!,
-                Source = error.Source,
-                StackTrace = error.StackTrace,
-                Layer= error.Layer,
-                Curl= error.Curl,
-                Route= error.Route
+                CreatedAt = errorLogDTO.CreatedAt,
+                Message = errorLogDTO.Message!,
+                Source = errorLogDTO.Source,
+                StackTrace = errorLogDTO.StackTrace,
+                Layer = errorLogDTO.Layer,
+                Curl = errorLogDTO.Curl,
+                Route = errorLogDTO.Route
             };
             try
             {
@@ -46,17 +37,14 @@ namespace SabzMarket.Infrastructure.Persistence.Repository
 
                 context.ErrorLogs.Add(errorLog);
                 await context.SaveChangesAsync();
-               
+
                 return errorLog.Id.ToString();
             }
             catch (Exception ex2)
             {
-               var result= await _fileLogService.SaveFailedLogAsync(ex2);
-                return result;  
+                var result = await _fileLogService.SaveFailedLogAsync(errorLogDTO);
+                return result;
             }
-
-
-
         }
     }
 }
