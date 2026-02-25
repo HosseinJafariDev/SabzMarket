5db2ea72f3522b21fd8b11bf1ae4514601887a7a
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-25 22:18:12 +0330

    Add GetAllSellerUseCase and IGetAllSellerUseCase

---
+++ Application/UseCases/FeaturedSellers/GetFeaturedSeller/GetAllSellerUseCase.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.IdentityModel.Tokens.Experimental;
+using SabzMarket.Application.Common;
+using SabzMarket.Application.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.FeaturedSellers.GetFeaturedSeller
+{
+    public class GetAllSellerUseCase : IGetAllSellerUseCase
+    {
+        private readonly IErrorRepository _errorRepository;
+        private readonly IFeaturedSellerQueryService _featuredSellerQueryService;
+        public GetAllSellerUseCase(IErrorRepository errorRepository, IFeaturedSellerQueryService featuredSellerQueryService)
+        {
+            _errorRepository = errorRepository;
+            _featuredSellerQueryService = featuredSellerQueryService;
+        }
+        public async Task<OperationResult<List<GetAllFeaturedSellerOutputDTO>>> ExecuteAsync()
+        {
+            try
+            {
+                var result = await _featuredSellerQueryService.SelectAllSellerAsync();
+                return OperationResult<List<GetAllFeaturedSellerOutputDTO>>.SuccessedResult(result);
+            }
+            catch (Exception ex)
+            {
+                var errorResult = await _errorRepository.LogErrorAsync(ex.ExceptionToErrorDTO(GetType().Name));
+                return OperationResult<List<GetAllFeaturedSellerOutputDTO>>.Failed(errorResult.ErrorMessage());
+            }
+        }
+    }
+}
---
+++ Application/UseCases/FeaturedSellers/GetFeaturedSeller/IGetAllSellerUseCase.cs
@@ -0,0 +1,14 @@
+﻿using SabzMarket.Application.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.FeaturedSellers.GetFeaturedSeller
+{
+    public interface IGetAllSellerUseCase
+    {
+        Task<OperationResult<List<GetAllFeaturedSellerOutputDTO>>> ExecuteAsync();
+    }
+}
