f3a1b7a22520adaa76b51093464c85fbefc5a2d2
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-25 22:05:45 +0330

    Add CheckoutOrderUseCase and ICheckoutOrderUseCase

---
+++ Application/UseCases/Orders/Checkout/CheckoutOrderUseCase.cs
@@ -0,0 +1,98 @@
+﻿using Application.Interfaces.Repositories;
+using Application.Interfaces.Services;
+using AutoMapper;
+using SabzMarket.Application.Common;
+using SabzMarket.Application.Interfaces.Persistence;
+using SabzMarket.Application.Interfaces.Repository;
+using SabzMarket.Application.UseCases.CartItems.GetCartItem;
+using SabzMarket.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.Orders.Checkout
+{
+    public class CheckoutOrderUseCase : ICheckoutOrderUseCase
+    {
+        private readonly ICartItemQueryService _cartItemQueryService;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ICartItemRepository _cartItemRepository;
+        private readonly IOrderRepository _orderRepository;
+        private readonly IOrderDetailRepository _orderDetailRepository;
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+        private readonly IErrorRepository _errorRepository;
+        public CheckoutOrderUseCase(
+            ICartItemQueryService cartItemQueryService,
+            IUnitOfWork unitOfWork,
+            ICartItemRepository cartItemRepository,
+            IOrderRepository orderRepository,
+            IOrderDetailRepository orderDetailRepository,
+            IProductRepository productRepository,
+            IMapper mapper,
+            IErrorRepository errorRepository)
+        {
+            _cartItemQueryService = cartItemQueryService;
+            _unitOfWork = unitOfWork;
+            _cartItemRepository = cartItemRepository;
+            _orderRepository = orderRepository;
+            _orderDetailRepository = orderDetailRepository;
+            _productRepository = productRepository;
+            _mapper = mapper;
+            _errorRepository = errorRepository;
+        }
+        public async Task<OperationResult> ExecuteAsync(long farmerId)
+        {
+            try
+            {
+                var cartItems = await _cartItemQueryService.SelectByFarmerIdAsync(farmerId);
+
+                var data = cartItems.Where(x => x.Quantity > x.ProducNumber).ToList();
+                foreach (var item in data)
+                {
+                    await _cartItemRepository.DeleteAsync(item.Id);
+                }
+                cartItems.RemoveAll(x => x.Quantity > x.ProducNumber);
+
+                if (!cartItems.Any())
+                {
+                    return OperationResult.FailedResult(Messages.CartEmpty);
+                }
+
+                await _unitOfWork.BeginAsync();
+
+                foreach (var item in cartItems)
+                {
+                    var checkOrder = await _orderRepository.CheckOrderAsync(farmerId, item.SellerId);
+                    if (!checkOrder)
+                    {
+                        var order = _mapper.Map<Order>(item);
+                        var resultOrder = await _orderRepository.InsertAsync(order);
+                        var orderDetail = _mapper.Map<OrderDetail>(item);
+                        await _orderDetailRepository.InsertAsync(orderDetail);
+                        await _productRepository.IncreaseNumberAsync(item.ProductId, -item.Quantity);
+                        await _cartItemRepository.DeleteAsync(item.Id);
+                    }
+                    else
+                    {
+                        var orderId = await _orderRepository.FindOrderByFarmerAndSellerAsync(farmerId, item.SellerId);
+                        var order = _mapper.Map<OrderDetail>(item);
+                        await _orderDetailRepository.InsertAsync(order);
+                        await _cartItemRepository.DeleteAsync(item.Id);
+                    }
+                }
+                await _unitOfWork.CommitAsync();
+                return OperationResult.SuccessedResult();
+            }
+            catch (Exception ex)
+            {
+                await _unitOfWork.RollbackAsync();
+                var erroeResult = await _errorRepository.LogErrorAsync(ex.ExceptionToErrorDTO(GetType().Name));
+                return OperationResult.Failed(erroeResult.ErrorMessage());
+            }
+
+        }
+    }
+}
---
+++ Application/UseCases/Orders/Checkout/ICheckoutOrderUseCase.cs
@@ -0,0 +1,14 @@
+﻿using SabzMarket.Application.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.Orders.Checkout
+{
+    public interface ICheckoutOrderUseCase
+    {
+        Task<OperationResult> ExecuteAsync(long farmerId);
+    }
+}
