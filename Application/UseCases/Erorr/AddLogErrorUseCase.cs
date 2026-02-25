d7f8762baf260d96ac8c0a4987f6a6c924135abb
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-25 22:36:07 +0330

    Add AddLogErrorUseCase and IAddLogErrorUseCase

---
+++ Application/UseCases/Erorr/AddLogErrorUseCase.cs
@@ -0,0 +1,24 @@
+﻿using SabzMarket.Application.Common;
+using SabzMarket.Application.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.Erorr
+{
+    public class AddLogErrorUseCase : IAddLogErrorUseCase
+    {
+        private readonly IErrorRepository _errorRepository;
+        public AddLogErrorUseCase(IErrorRepository errorRepository)
+        {
+            _errorRepository = errorRepository;
+        }
+        public async Task<OperationResult> ExecuteAsync(ErrorLogDTO errorLogDTO)
+        {
+            var errorResult = await _errorRepository.LogErrorAsync(errorLogDTO);
+            return OperationResult.SuccessedResult(true, errorResult.ErrorMessage());
+        }
+    }
+}
