581fc57abd1d830a9a506e8f2404bf070197ccb2
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-25 22:32:46 +0330

    Add CartItemProfile

---
+++ Application/UseCases/CartItems/Mappers/CartItemProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using SabzMarket.Application.UseCases.CartItems.AddToCart;
+using SabzMarket.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.CartItems.Mappers
+{
+    public class CartItemProfile : Profile
+    {
+        public CartItemProfile()
+        {
+            CreateMap<AddToCartInputDTO, CartItem>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+        }
+    }
+}
