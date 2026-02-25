a1e6a52d889dd0e148b8bdb49da7a5073467402f
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-25 22:13:03 +0330

    Add GetPendingOrdersForSellerUseCase and IGetPendingOrdersForSellerUseCase

---
+++ Application/UseCases/Orders/GetOrders/GetPendingOrdersForSellerUseCase.cs
@@ -0,0 +1,35 @@
+﻿using SabzMarket.Application.Common;
+using SabzMarket.Application.Interfaces.Repository;
+using SabzMarket.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.Orders.GetOrders
+{
+    public class GetPendingOrdersForSellerUseCase : IGetPendingOrdersForSellerUseCase
+    {
+        private readonly IOrderQueryService _orderQueryService;
+        private readonly IErrorRepository _errorRepository;
+        public GetPendingOrdersForSellerUseCase(IOrderQueryService orderQueryService, IErrorRepository errorRepository)
+        {
+            _errorRepository = errorRepository;
+            _orderQueryService = orderQueryService;
+        }
+        public async Task<OperationResult<List<GetOrdersForSellerOutputDTO>>> ExecuteAsync(long sellerId, string search)
+        {
+            try
+            {
+                var orders = await _orderQueryService.SelectPendingOrdersForSellerAsync(sellerId, search);
+                return OperationResult<List<GetOrdersForSellerOutputDTO>>.SuccessedResult(orders);
+            }
+            catch (Exception ex)
+            {
+                var errorResult = await _errorRepository.LogErrorAsync(ex.ExceptionToErrorDTO(GetType().Name));
+                return OperationResult<List<GetOrdersForSellerOutputDTO>>.Failed(errorResult.ErrorMessage());
+            }
+        }
+    }
+}
