405e0b7da954098f2a691f50bf3324462443e79d
Hosein <hoseinjaf84ri84@gmail.com>
2026-02-25 22:18:23 +0330

    Add IFeaturedSellerQueryService

---
+++ Application/UseCases/FeaturedSellers/GetFeaturedSeller/IFeaturedSellerQueryService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Application.UseCases.FeaturedSellers.GetFeaturedSeller
+{
+    public interface IFeaturedSellerQueryService
+    {
+        Task<List<GetAllFeaturedSellerOutputDTO>> SelectAllSellerAsync();
+    }
+}
