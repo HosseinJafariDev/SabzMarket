a8e18ca496efcb6016dc2474aea39cfc1eb09bb0
Hosein <hoseinjaf84ri84@gmail.com>
2026-04-28 12:44:40 +0330

    refactor Chat DomainEntity

--- SabzMarket.Domain/Entities/Chat.cs
+++ SabzMarket.Domain/Entities/Chat.cs
@@ -8,9 +8,13 @@ namespace SabzMarket.Domain.Entities
 {
     public class Chat
     {
-            public long Id { get; set; }
-            public string? Message { get; set; }
-            public long FromUserId { get; set; }
-            public long ToUserId { get; set; }
+        public long Id { get; set; }
+        public string? Message { get; set; }
+        public long FromUserId { get; set; }
+        public long ToUserId { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime SentAt { get; set; }
+        public bool IsFile { get; set; }
     }
 }
