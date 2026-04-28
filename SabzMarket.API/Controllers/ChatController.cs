1c64de059512c9c546828eb8eba05038f118c236
Hosein <hoseinjaf84ri84@gmail.com>
2026-04-28 12:51:28 +0330

    feat: implement chat system with repository, query service, use cases, and API controller

---
+++ SabzMarket.API/Controllers/ChatController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SabzMarket.Application.Common;
+using SabzMarket.Application.UseCases.Chats.findUsersChatted;
+using SabzMarket.Application.UseCases.Chats.GetMessage;
+using SabzMarket.Application.UseCases.Chats.SendMessage;
+
+namespace SabzMarket.API.Controllers
+{
+    public class ChatController : BaseController
+    {
+        private readonly IfindUsersChattedWithIdUseCase _findUsersChatted;
+        private readonly IGetMessageUseCase _getMessage;
+        private readonly ISendMessageUseCase _sendMessage;
+        public ChatController(
+            IfindUsersChattedWithIdUseCase findUsersChatted,
+            IGetMessageUseCase getMessage,
+            ISendMessageUseCase sendMessage)
+        {
+            _findUsersChatted = findUsersChatted;
+            _getMessage = getMessage;
+            _sendMessage = sendMessage;
+        }
+        [HttpGet]
+        public async Task<OperationResult<List<findUsersChattedOutputDTO>>> FindUsersChattedWithIdAsync(long id)
+        {
+            var result = await _findUsersChatted.ExecuteAsync(id);
+            return result;
+        }
+        [HttpGet]
+        public async Task<OperationResult<List<GetMessageOutputDTO>>> GetMessageAsync(long fromId, long toId)
+        {
+            var result = await _getMessage.ExecuteAsync(fromId, toId);
+            return result;
+        }
+        [HttpPost]
+        public async Task<OperationResult> SendMessageAsync(SendMessageInputDTO sendMessageInputDTO)
+        {
+            var result = await _sendMessage.ExecuteAsync(sendMessageInputDTO);
+            return result;
+        }
+    }
+}
