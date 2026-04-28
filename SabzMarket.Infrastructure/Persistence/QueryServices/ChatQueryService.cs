27d029339f7ab2b7786ca1ef45a65b37215ac74d
Hosein <hoseinjaf84ri84@gmail.com>
2026-04-28 12:32:39 +0330

    feat:add Chat QueryService

---
+++ SabzMarket.Infrastructure/Persistence/QueryServices/ChatQueryService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SabzMarket.Application.UseCases.Chats.findUsersChatted;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Infrastructure.Persistence.QueryServices
+{
+    public class ChatQueryService : IChatQueryService
+    {
+        private readonly SabzMarketDbContext _dbContext;
+        public ChatQueryService(SabzMarketDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<List<findUsersChattedOutputDTO>> findUsersChattedWith(long id)
+        {
+            var result = await _dbContext
+                .Chats
+                .AsNoTracking()
+                .Where(x => x.FromUserId == id || x.ToUserId == id)
+                .Select(x => new findUsersChattedOutputDTO()
+                {
+                    Id = x.FromUserId == id ? x.ToUser!.Id : x.FromUser!.Id,
+                    Firstname = x.FromUserId == id ? x.ToUser!.FirstName : x.FromUser!.FirstName,
+                    Lastname = x.FromUserId == id ? x.ToUser!.LastName : x.FromUser!.LastName,
+                    Username = x.FromUserId == id ? x.ToUser!.UserName : x.FromUser!.UserName,
+                })
+                .ToListAsync();
+            return result;
+        }
+    }
+}
