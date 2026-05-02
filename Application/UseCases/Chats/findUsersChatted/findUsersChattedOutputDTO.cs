54df2c666c136e54029752378ed0de97d9947407
Hosein <hoseinjaf84ri84@gmail.com>
2026-05-02 12:01:54 +0330

    add ProfileImage

--- Application/UseCases/Chats/findUsersChatted/findUsersChattedOutputDTO.cs
+++ Application/UseCases/Chats/findUsersChatted/findUsersChattedOutputDTO.cs
@@ -12,5 +12,6 @@ namespace SabzMarket.Application.UseCases.Chats.findUsersChatted
         public string? Username { get; set; }
         public string? Firstname { get; set; }
         public string? Lastname { get; set; }
+        public string? ProfileImage { get; set; }
     }
 }
