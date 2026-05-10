97830db39406f4476409a22208bf8e4cd4b2f65f
hosein <hoseinjaf84ri84@gmail.com>
2026-05-10 13:56:25 +0330

    refactor: centralize entity configurations using ApplyConfigurationsFromAssembly

--- SabzMarket.Infrastructure/Persistence/SabzMarketDbContext.cs
+++ SabzMarket.Infrastructure/Persistence/SabzMarketDbContext.cs
@@ -30,65 +30,7 @@ namespace SabzMarket.Infrastructure.Persistence
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<CategorieTable>().ToTable("Categorie")
-                .HasData(
-                new CategorieTable { Id = 1, Name = "کود های شیمیایی" },
-                new CategorieTable { Id = 2, Name = "کود های آلی " },
-                new CategorieTable { Id = 3, Name = "کودهای بیولوژیک" },
-                new CategorieTable { Id = 4, Name = "حشره کش ها " },
-                new CategorieTable { Id = 5, Name = "علف کش ها" },
-                new CategorieTable { Id = 6, Name = "سموم معدنی " },
-                new CategorieTable { Id = 7, Name = "سموم آلی " }
-                );
-            modelBuilder.Entity<SellerTable>().ToTable("Seller")
-                .HasOne(s => s.User)
-                .WithOne(u => u.Seller)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<FarmerTable>().ToTable("Farmer")
-                .HasOne(f => f.User)
-                .WithOne(u => u.Farmer)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<OrderDetailTable>().ToTable("OrderDetail")
-                .HasOne(od => od.Product)
-                .WithMany(p => p.OrderDetails)
-                .HasForeignKey(od => od.ProductId)
-                .OnDelete(DeleteBehavior.Restrict);
-            modelBuilder.Entity<ChatTable>().ToTable("Chat")
-                .HasOne(c => c.FromUser)
-                .WithMany()
-                .HasForeignKey(c => c.FromUserId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<ChatTable>()
-                .HasOne(c => c.ToUser)
-                .WithMany()
-                .HasForeignKey(c => c.ToUserId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<ChatTable>()
-                .HasIndex(c => c.FromUserId)
-                .HasDatabaseName("IX_Chats_FromUserId");
-
-            modelBuilder.Entity<ChatTable>()
-                .HasIndex(c => c.ToUserId)
-                .HasDatabaseName("IX_Chats_ToUserId");
-
-            modelBuilder.Entity<FeaturedSellerTable>().ToTable("FeaturedSeller")
-                .HasIndex(x => x.SellerId)
-                .IsUnique();
-
-            modelBuilder.Entity<UserTable>().ToTable("User");
-
-            modelBuilder.Entity<CartItemTable>().ToTable("CartItem");
-
-            modelBuilder.Entity<ErrorTable>().ToTable("Error");
-
-            modelBuilder.Entity<OrderTable>().ToTable("Order");
-
-            modelBuilder.Entity<ProductTable>().ToTable("Product");
-
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(SabzMarketDbContext).Assembly);
         }
     }
 }
