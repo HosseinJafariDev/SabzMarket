303c3e0c262e993f557e7ced21657f10a4a43edf
hosein <hoseinjaf84ri84@gmail.com>
2026-06-05 23:02:02 +0330

    feat: add OperationError

---
+++ SabzMarket.Domain/Enums/OperationError.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Domain.Enums
+{
+    public enum OperationError
+    {
+        [Display(Name = "عملیات با موفقیت انجام شد")]
+        Success,
+
+        [Display(Name = "عملیات با موفقیت انجام شد با متن دلخواه")]
+        None,
+
+        [Display(Name = "خطایی در سرور رخ داده است")]
+        ServerError,
+
+        [Display(Name = "یافت نشد")]
+        NotFound,
+
+        [Display(Name = "اطلاعات معتبر نمی باشد")]
+        Validation,
+
+        [Display(Name = "عملیات نمی‌تواند انجام شود چون با یک وضعیت موجود در سیستم در تضاد است")]
+        Conflict,
+
+        [Display(Name = "خطای سطح دسرسی")]
+        Forbidden,
+
+        [Display(Name = "خطای احراز هویت")]
+        Unauthorized,
+    }
+}
