f0e35a03613e17125c0c5c80e6aae54e232a481c
hosein <hoseinjaf84ri84@gmail.com>
2026-06-05 23:02:55 +0330

    feat:  add AppException

---
+++ SabzMarket.Domain/Exceptions/ConflictException.cs
@@ -0,0 +1,22 @@
+﻿using SabzMarket.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Domain.Exceptions
+{
+    public class ConflictException : AppException
+    {
+        public ConflictException(string message)
+            : base(message, OperationError.Conflict)
+        {
+        }
+
+        public ConflictException(string message, Exception exception)
+            : base(message, OperationError.Conflict, exception)
+        {
+        }
+    }
+}
---
+++ SabzMarket.Domain/Exceptions/NotFoundException.cs
@@ -0,0 +1,22 @@
+﻿using SabzMarket.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabzMarket.Domain.Exceptions
+{
+    public class NotFoundException : AppException
+    {
+        public NotFoundException(string message)
+            : base(message, OperationError.NotFound)
+        {
+        }
+
+        public NotFoundException(string message, Exception exception)
+            : base(message, OperationError.NotFound, exception)
+        {
+        }
+    }
+}
