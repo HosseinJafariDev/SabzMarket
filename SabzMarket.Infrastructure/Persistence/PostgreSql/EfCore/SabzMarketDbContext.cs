39cbd92df73a3fe9bba2e3bfbc8c8715cde10c90
hossein <hoseinjaf84ri84@gmail.com>
2026-08-28 23:49:51 +0330

    [REFACTOR]: MOVE CLASSES TO NEW FOLDERS

--- SabzMarket.Infrastructure/Persistence/PostgreSql/EfCore/SabzMarketDbContext.cs
+++ SabzMarket.Infrastructure/Persistence/PostgreSql/EfCore/SabzMarketDbContext.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SabzMarket.Infrastructure.Persistence
+namespace SabzMarket.Infrastructure.Persistence.Postgresql.EfCore
 {
     public class SabzMarketDbContext : DbContext
     {
