ff9769db41bd38aa172f0d3ad9d1e1b10e1a71d5
hossein <hoseinjaf84ri84@gmail.com>
2026-08-31 02:02:16 +0330

    [REFACTOR]: Refactor Select Method

--- Application/Interfaces/Persistence/IRepository.cs
+++ Application/Interfaces/Persistence/IRepository.cs
@@ -1,9 +1,19 @@
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.Query;
+
 namespace SabzMarket.Application.Interfaces.Persistence;
 
 public interface IRepository<TEntity, in TKey> where TEntity : class
 {
-    Task<TEntity?> GetByIdAsync(TKey id, CancellationToken cancellationToken);
-    Task<IReadOnlyList<TEntity>> GetAllAsync(CancellationToken cancellationToken);
+    Task<TEntity?> GetByIdAsync(TKey id,
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+        bool tracking = false, CancellationToken cancellationToken = default);
+
+    Task<IReadOnlyList<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>>? where = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+        bool tracking = false, CancellationToken cancellationToken = default);
+
     void Add(TEntity entity);
     void Update(TEntity entity);
     void Remove(TEntity entity);
